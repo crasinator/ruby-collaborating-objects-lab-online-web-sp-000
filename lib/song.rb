9b432abc50d4bcf414e2982f944cbb93ed396fc9
Jordan Reed <jordanreed03@gmail.com>
2021-01-07 03:15:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
   end
 
   def self.new_by_filename(file)
-    str = file.chomp(".mp3")
+    str = file.chomp(".mp3").flatten
     song_info = str.split(" - ")
     song = Song.new(song_info[1])
     song.artist_name = song_info[0]

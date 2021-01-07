ae72a618cb3ea1ffaa827b11c0ef02bf0be19fdb
Jordan Reed <jordanreed03@gmail.com>
2021-01-07 04:41:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,9 @@ class Song
     song = self.new(splt_name[1])
     artist = Artist.find_or_create_by_name(splt_name[0])
     song.artist = artist 
-    
+    artist.add_song (song)
+    song 
+  end 
   
   def self.all 
     @@all 

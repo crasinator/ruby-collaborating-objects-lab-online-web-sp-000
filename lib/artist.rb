8922e8760552e1071c2a64a573c3754049b993a9
Jordan Reed <jordanreed03@gmail.com>
2021-01-07 01:08:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,6 +5,7 @@ class Artist
   def initialize(name)
     @name = name
     @songs = [ ]
+    save
   end
 
   def add_song(song)
@@ -13,7 +14,8 @@ class Artist
 
   def save
     @@all << self   
-end 
+  end 
+  
   def self.all      
     @@all
   end

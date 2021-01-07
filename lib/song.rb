469ca03ca899fad102398c9a94c75509940c6805
Jordan Reed <jordanreed03@gmail.com>
2021-01-07 04:56:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,11 +8,6 @@ class Song
     @@all << self
   end
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
-
   def self.new_by_filename(filename)
     splt_name = filename.split(" - ")
     song = self.new(splt_name[1])
@@ -21,6 +16,11 @@ class Song
     
     song 
   end 
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
   
   def self.all 
     @@all 

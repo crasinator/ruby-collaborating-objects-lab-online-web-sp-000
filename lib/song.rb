13464bef712a8fb33765c4bc128973513bf333c8
Jordan Reed <jordanreed03@gmail.com>
2021-01-07 01:22:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,11 @@
 class Song
   attr_accessor :artist, :name
 
+@@all = [ ]
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def artist_name=(name)
@@ -16,4 +19,8 @@ class Song
     song.artist_name = song_info[0]
     song
   end
+  
+  def self.all 
+    @@all 
+  end 
 end
